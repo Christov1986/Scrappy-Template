9bce5e214643dec049c8339a5a0483fc8e22ba43
christov <cvh@cerberusfs.com>
2020-03-19 09:30:41 +0200

    Completed the spider to crawl the test website and extract the data of all 1000 books

--- books/books/spiders/spider.py
+++ books/books/spiders/spider.py
@@ -8,13 +8,48 @@ class SpiderSpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        all_the_books = response.xpath('//article')
+        all_the_books = response.xpath('//article[@class="product_pod"]')
 
         for books in all_the_books:
-            title = books.xpath('.//h3/a/@title').extract_first()
-            price = books.xpath('.//div[@class="product_price"]/p[@class="price_color"]/text()').extract_first()
-            image_url = self.start_urls[0] + books.xpath('.//img/@src').extract_first()
             book_url = self.start_urls[0] + books.xpath('.//div[@class="image_container"]/a/@href').extract_first()
-            print(title + " @ " + price)
-            print(image_url)
-            print(book_url)
+
+            if 'catalogue/' not in book_url:
+                book_url = self.start_urls[0] + 'catalogue/' + \
+                           books.xpath('.//div[@class="image_container"]/a/@href').extract_first()
+
+            yield scrapy.Request(book_url, callback=self.parse_book)
+
+        next_page_url = self.start_urls[0] + response.xpath('//li[@class="next"]/a/@href').extract_first()
+
+        if 'catalogue/' not in next_page_url:
+            next_page_url = self.start_urls[0] + 'catalogue/' + \
+                            response.xpath('//li[@class="next"]/a/@href').extract_first()
+
+        yield scrapy.Request(next_page_url, callback=self.parse)
+
+    def parse_book(self, response):
+        title = response.xpath('//div/h1/text()').extract_first()
+        image_url = self.start_urls[0] + response.xpath('//div/img/@src').extract_first().replace('../../', '')
+        price = response.xpath('//div[contains(@class, "product_main")]/p[@class="price_color"]/text()').extract_first()
+        stock = response.xpath('//div[contains(@class, "product_main")]/'
+                               'p[@class="instock availability"]/text()').extract()[1].strip()
+        star_rating = response.xpath('//div[contains(@class, "product_main")]/p[contains(@class, "star-rating")]/'
+                                     '@class').extract_first().replace('star-rating ', '')
+        description = response.xpath('//div[@id="product_description"]/following-sibling::p/text()').extract_first()
+        upc_code = response.xpath('//tr[1]/td/text()').extract_first()
+        price_excl = response.xpath('//tr[3]/td/text()').extract_first()
+        price_incl = response.xpath('//tr[4]/td/text()').extract_first()
+        tax = response.xpath('//tr[5]/td/text()').extract_first()
+
+        yield{
+        'Title' : title,
+        'Image URL' : image_url,
+        'Price' : price,
+        'Stock level' : stock,
+        'Star rating' : star_rating,
+        'Description' : description,
+        'UPC code' : upc_code,
+        'Price excluding tax' : price_excl,
+        'Price including tax' : price_incl,
+        'Tax' : tax
+        }
